87eb91089628e002ec1e6a504da88340424bd2eb
infoMENTUM\Harish.Gowdra <harish.gowdra@infomentum.co.uk>
2018-11-17 17:21:12 +0000

    Added initial files

---
+++ src/main/java/hari/springframework/sbpetclinic/SbPetClinicApplication.java
@@ -0,0 +1,12 @@
+package hari.springframework.sbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPetClinicApplication.class, args);
+	}
+}
